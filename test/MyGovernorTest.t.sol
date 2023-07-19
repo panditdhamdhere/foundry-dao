6604f8942e300d828202a0c4b969e10912b46445
panditdhamdhere <dhamdherepandit@gmail.com>
2023-07-19 13:04:47 +0530

    propose, vote, queue added

--- test/MyGovernorTest.t.sol
+++ test/MyGovernorTest.t.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.19;
 
-import {Test} from "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
 import {MyGovernor} from "../src/MyGovernor.sol";
 import {GovToken} from "../src/GovToken.sol";
 import {TimeLock} from "../src/TimeLock.sol";
@@ -21,7 +21,13 @@ contract MyGovernorTest is Test {
     address[] proposers;
     address[] executors;
 
+    uint256[] values;
+    bytes[] calldatas;
+    address[] targets;
+
     uint256 public constant MIN_DELAY = 3600; //  1 hour = after a vote passes
+    uint256 public constant VOTING_DELAY = 1; // How many blocks till a vote is active
+    uint256 public constant VOTING_PERIOD = 50400; //
 
     function setUp() public {
         govToken = new GovToken();
@@ -49,6 +55,49 @@ contract MyGovernorTest is Test {
     function testCantUpdateBoxWithoutGovernance() public {
         vm.expectRevert();
         box.store(1);
-        
+    }
+
+    function testGovernanceUpdatesBox() public {
+        uint256 valueToStore = 888;
+        string memory description = "store 1 in Box";
+        bytes memory encodedFunctionCall = abi.encodeWithSignature(
+            "store(uint256)",
+            valueToStore
+        );
+        values.push(0);
+        calldatas.push(encodedFunctionCall);
+        targets.push(address(box));
+
+        // 1. Propose to the Dao.
+        uint256 proposalId = governor.propose(
+            targets,
+            values,
+            calldatas,
+            description
+        );
+
+        // view the state
+        console.log("Proposal State: ", uint256(governor.state(proposalId)));
+
+        vm.warp(block.timestamp + VOTING_DELAY + 1);
+        vm.warp(block.number + VOTING_DELAY + 1);
+
+        console.log("Proposal State: ", uint256(governor.state(proposalId)));
+
+        // 2. Vote
+        string memory reason = "cuz Pandit is cool";
+
+        uint8 voteWay = 1; // voting yes
+        vm.prank(USER);
+        governor.castVoteWithReason(proposalId, voteWay, reason);
+
+        vm.warp(block.timestamp + VOTING_PERIOD + 1);
+        vm.warp(block.number + VOTING_PERIOD + 1);
+
+        // 3. Queue the TX
+        bytes32 descriptionHash = keccak256(abi.encodePacked(description));
+        governor.queue(targets, values, calldatas, descriptionHash);
+
+        // 4. Execute
     }
 }
